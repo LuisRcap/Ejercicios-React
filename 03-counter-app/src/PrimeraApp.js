f8872649a3a6d53be467ef1c6678c6a8fb775270
LuisRcap <luisdany7773@gmail.com>
2021-09-08 22:09:08 -0500

    Props

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,13 +1,12 @@
 import React from "react";
 // import React, { Fragment } from "react";
 
-const PrimeraApp = () => {
+const PrimeraApp = ( { saludo = "Hola Mundo"} ) => {
     
-    const saludo = 'Hola Mundo';
 
     return (
         <>
-            <h1>{saludo}</h1>
+            <h1>{ saludo }</h1>
             {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> //Esto es para imprimir json */}
             <p>Mi primera aplicación</p>
         </>
