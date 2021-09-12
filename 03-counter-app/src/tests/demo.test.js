9606b90395bdc8877be91f2a3b790f8e94a54065
LuisRcap <luisdany7773@gmail.com>
2021-09-11 21:59:01 -0500

    Jest - Expect y toBe

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,5 +1,15 @@
-test( 'Debe ser true', () => {
-    const isActive = true;
-
-    if( isActive ) throw new Error('No está activo'); 
+describe('Pruebas en el archivo demo.test.js', () => {
+    
+    test( 'Deben de ser iguales los string', () => {
+        // 1. Inicialización
+        const mensaje = 'Hola mundo';
+    
+        // 2. Estímulo
+        const mensaje2 = 'Hola mundo';
+    
+        // 3. Observar el comportamiento
+        expect( mensaje ).toBe( mensaje2 ); // ===
+    });
+    
 });
+
