172acd8f366d8a7d56cb5ceca30a24f136112edd
LuisRcap <luisdany7773@gmail.com>
2021-09-24 01:37:28 -0500

    Pruebas en 08 arreglos

--- 03-counter-app/src/base/08-imp-exp.js
+++ 03-counter-app/src/base/08-imp-exp.js
@@ -1,16 +1,8 @@
 //import { heroes } from './data/heroes';
 //import { heroes } from './data/heroes';
 // import { heroes } from "./data/heroes";
-import heroes /*, { owners }*/ from "../data/heroes"; // Importación por default
-
-// console.log(owners);
-
+import heroes from "../data/heroes"; // Importación por default
 
 export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id);
 
-// console.log(getHeroeByID(2));
-
 export const getHeroesByOwner = (owner) => heroes.filter( (heroe) => heroe.owner === owner);
-
-// console.log(getHeroesByOwner('Marvel'));
-// export default getHeroeByID;
