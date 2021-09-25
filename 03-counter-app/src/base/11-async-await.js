35a2e0b7f1e3b58df6dd20abe90261c7d5db1bac
LuisRcap <luisdany7773@gmail.com>
2021-09-25 02:47:01 -0500

    Pruebas Async-Await

--- 03-counter-app/src/base/11-async-await.js
+++ 03-counter-app/src/base/11-async-await.js
@@ -1,19 +1,18 @@
-const getImagen = async() => {
+export const getImagen = async() => {
 
     try{
         const apikey = 'Pozh5uZofEN9CXolqKGTTcSXfGXdytxX';
         const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apikey}`);
 
         const { data } = await resp.json();
-
         const { url } = data.images.original;
-        const img = document.createElement('img');
-        img.src = url;
 
-        document.body.append(img);
+        return url;
+
     }catch(err){
         // Manejo del error
-        console.error(err);
+        // console.error(err);
+        return 'No existe';
     }
 
 };
