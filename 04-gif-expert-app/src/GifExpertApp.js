bac8fe73ba3cafe370b5a4b2a994a3b59657e7b7
LuisRcap <luisdany7773@gmail.com>
2021-09-26 18:50:10 -0500

    GifExpertApp creado

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const GifExpertApp = () => {
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr />
+        </>
+    );
+};
+
+export default GifExpertApp;
