6f18b97a7c87993b0234233a30117fcbd89ee75d
LuisRcap <luisdany7773@gmail.com>
2021-09-26 20:02:13 -0500

    Lista categorias

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 
 const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+        setCategories( ['Naruto Shippuden', ...categories] );
+    };
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick={ handleAdd }>Agregar</button>
+
+            <ol>
+                {
+                    categories.map( (category, i) => {
+                        return <li key={ category } >{ category }</li>;
+                    })
+                }
+            </ol>
         </>
     );
 };
