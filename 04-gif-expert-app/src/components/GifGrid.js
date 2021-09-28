43654775d591df4ce01380b2cc332b38389c9231
LuisRcap <luisdany7773@gmail.com>
2021-09-27 19:49:15 -0500

    useEffect :bulb:

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const GifGrid = ({ category }) => {
+
+    const [count, setCount] = useState(1);
+
+    useEffect( () => {
+        getGifs();
+    }, []);
 
     const getGifs = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=Pozh5uZofEN9CXolqKGTTcSXfGXdytxX';
@@ -19,11 +25,11 @@ const GifGrid = ({ category }) => {
         console.log(gifs);
     };
 
-    getGifs();
-
     return (
         <>
             <h3>{ category }</h3>
+            <h3>{ count }</h3>
+            <button onClick={ () => setCount( count + 1 ) }>+1</button>
         </>
     );
 };
