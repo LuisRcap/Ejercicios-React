23eca4b1cb704f491252ca5da29722bbaa271058
LuisRcap <luisdany7773@gmail.com>
2021-09-28 23:35:18 -0500

    Obtener imagenes y bandera de carga :racehorse:

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,36 +1,29 @@
 import React/* , { useState, useEffect } */ from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs';
-// import { getGifs } from '../helpers/getGifs';
-// import GifGridItem from './GifGridItem';
+import GifGridItem from './GifGridItem';
 
 const GifGrid = ({ category }) => {
 
-    // const [images, setImages] = useState([]);
-    const {data, loading} = useFetchGifs();
-
-    console.log(loading);
-    console.log(data);
-    /*
-    useEffect( () => {
-        getGifs( category )
-            .then( setImages );
-    }, [ category ]); */
+    const {data:images, loading} = useFetchGifs( category );    
 
     return (
         <>
             <h3>{ category }</h3>
 
-            { loading ? 'Cargando...' : 'Data cargada '}
-            {/* <div className="card-grid">
-                {
-                    images.map( ( img ) =>(
-                        <GifGridItem
-                            key={img.id}
-                            { ...img }
-                        />
-                    ))
-                }
-            </div> */}
+            { loading && <p>Loading</p> }
+
+            {
+                <div className="card-grid">
+                    {
+                        images.map( ( img ) =>(
+                            <GifGridItem
+                                key={img.id}
+                                { ...img }
+                            />
+                        ))
+                    }
+                </div>
+            }
         </>
     );
 };
