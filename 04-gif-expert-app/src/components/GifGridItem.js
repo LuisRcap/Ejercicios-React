d02773d752df0c63f04cba0e64a08f862a870d7b
LuisRcap <luisdany7773@gmail.com>
2021-10-03 22:46:23 -0500

    PropTypes agregados :gear:

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const GifGridItem = ({title, url}) => {
 
@@ -7,7 +8,12 @@ const GifGridItem = ({title, url}) => {
             <img src={ url } alt={title} />
             <p>{ title }</p>
         </div>
-    )
-}
+    );
+};
+
+GifGridItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+};
 
 export default GifGridItem;
--- 04-gif-expert-app/src/tests/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/GifGridItem.test.js
@@ -4,10 +4,16 @@ import GifGridItem from "../components/GifGridItem";
 
 describe('Pruebas de <GifGridItem />', () => {
 
+    const title = 'Un titulo';
+    const url = 'https://localhost/algo.jpg';
+
     test('Debe mostrar el componente correctamente', () => {
-        const wrapper = shallow( <GifGridItem /> );
+        const wrapper = shallow( <GifGridItem
+                title={title}
+                url={url}
+            /> );
 
         expect( wrapper ).toMatchSnapshot();
-    })
+    });
     
-})
+});
