3ea96f4985e9f82ff2a4ae0095b9a850e6599cb7
LuisRcap <luisdany7773@gmail.com>
2021-10-11 23:26:07 -0500

    Test del componente :test_tube:

--- 04-gif-expert-app/src/tests/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/GifGridItem.test.js
@@ -6,14 +6,41 @@ describe('Pruebas de <GifGridItem />', () => {
 
     const title = 'Un titulo';
     const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow( <GifGridItem
+            title={title}
+            url={url}
+        /> );
 
     test('Debe mostrar el componente correctamente', () => {
-        const wrapper = shallow( <GifGridItem
-                title={title}
-                url={url}
-            /> );
 
         expect( wrapper ).toMatchSnapshot();
     });
+
+    test('Debe de tener un párrafo con el title', () => {
+        
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe( title );
+
+    });
+
+    test('Debe tener la imagen igual al url y alt de los props', () => {
+       
+        const img = wrapper.find('img');
+        // console.log( img.prop('src') );
+
+        expect( img.prop('src') ).toBe(url);
+        expect( img.prop('alt') ).toBe(title);
+    });
+    
+    test('Debe tener animate__fadeIn', () => {
+        
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        
+        expect( className.includes('animate__fadeIn') ).toBe( true );
+
+    });
+    
+    
     
 });
