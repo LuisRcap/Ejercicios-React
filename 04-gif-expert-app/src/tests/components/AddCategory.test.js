70243018dbb5deb080d107a4cbbb493de562f89e
LuisRcap <luisdany7773@gmail.com>
2021-10-13 13:18:57 -0500

    Prueba en AddCategory :test_tube:

---
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AddCategory from '../../components/AddCategory';
+
+describe('Pruebas en <AddCategory />', () => {
+    
+    const setCategories = () => {};
+
+    test('Debe de mostrarse correctamente', () => {
+        
+        const wrapper = shallow( <AddCategory setCategories={ setCategories } /> );
+
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+    
+
+});
