a7b9fadc71ed4ac90b86b3512f68879d1bbc5f0e
LuisRcap <luisdany7773@gmail.com>
2021-10-13 23:08:40 -0500

    test agregado en AddCategory :gear:

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -5,12 +5,22 @@ import AddCategory from '../../components/AddCategory';
 describe('Pruebas en <AddCategory />', () => {
     
     const setCategories = () => {};
+    const wrapper = shallow( <AddCategory setCategories={ setCategories } /> );
 
     test('Debe de mostrarse correctamente', () => {
-        
-        const wrapper = shallow( <AddCategory setCategories={ setCategories } /> );
-
+    
         expect( wrapper ).toMatchSnapshot();
+
+    });
+    
+    test('Debe de cambiar la caja de texto', () => {
+        
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+
+        input.simulate('change', { target: { value } });
+
+        expect( wrapper.find('p').text().trim() ).toBe( value );
 
     });
     
