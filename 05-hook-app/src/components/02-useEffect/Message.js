4af4160e88d586063f731ed8cb2f5477925f311a
LuisRcap <luisdany7773@gmail.com>
2021-11-01 18:22:15 -0600

    useEffect cleanup :brain:

---
+++ 05-hook-app/src/components/02-useEffect/Message.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+
+const Message = () => {
+
+    useEffect(() => {
+        
+        console.log("Componente montado");
+
+        return () => {
+            console.log("Componente desmontado");
+        }
+    }, [])
+
+    return (
+        <>
+            <h3>Eres genial!</h3>
+        </>
+    )
+}
+
+export default Message;
--- 05-hook-app/src/components/02-useEffect/SimpleForm.js
+++ 05-hook-app/src/components/02-useEffect/SimpleForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './effects.css';
+import Message from './Message';
 
 const SimpleForm = () => {
 
@@ -11,15 +12,15 @@ const SimpleForm = () => {
     const { name, email } = formState;
 
     useEffect( () => {
-        console.log('Hey!');
+        // console.log('Hey!');
     }, [] );
 
     useEffect( () => {
-        console.log('formState cambió');
+        // console.log('formState cambió');
     }, [formState] );
 
     useEffect( () => {
-        console.log('email cambió');
+        // console.log('email cambió');
     }, [email] );
 
     const handleInputChange = ({ target }) => {
@@ -59,6 +60,8 @@ const SimpleForm = () => {
                     onChange={ handleInputChange }
                 />
             </div>
+
+            { (name === '123') && <Message />}
 
         </>
     )
