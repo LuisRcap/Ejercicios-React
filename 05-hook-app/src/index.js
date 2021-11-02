256c9b1ce1be157caeddb1a727f16e75a660d9b0
LuisRcap <luisdany7773@gmail.com>
2021-11-01 20:30:46 -0600

    useFetch - CustomHook :sparkles:

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
+// import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 // import HookApp from './HookApp';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
@@ -9,7 +10,7 @@ import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 
 ReactDOM.render(
   
-    <FormWithCustomHook />,
+    <MultipleCustomHooks />,
   document.getElementById('root')
 
 );
