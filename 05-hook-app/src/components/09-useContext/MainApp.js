702a8461d03d1e3dbbcbf138e6584c6211d4293f
LuisRcap <luisdany7773@gmail.com>
2021-11-24 15:09:31 -0600

    createContext y useContext :brain:

--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import { UserContext } from './UserContext';
 
 const MainApp = () => {
+
+    const user = {
+        id: 1234,
+        name: 'Luis',
+        email: 'luisr@gmail.com'
+    };
+
     return (
-        <AppRouter />
+        <UserContext.Provider value={ user } >
+            <AppRouter />
+        </UserContext.Provider>
     )
 };
 
