1d7ea99e722021be494ce1b2215f1985a18b0602
LuisRcap <luisdany7773@gmail.com>
2022-01-30 22:23:12 -0600

    SearchComponent :fire:

--- 07-heroes-app/src/components/search/SearchScreen.js
+++ 07-heroes-app/src/components/search/SearchScreen.js
@@ -1,6 +1,45 @@
+import { useForm } from "../../hooks/useForm";
+
 const SearchScreen = () => {
+
+    const [ { searchText }, handleInputChange ] = useForm({
+        searchText: ''
+    })
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchText)
+    }
+
     return (
-        <h1>Search Screen</h1>
+        <>
+            <h1>Búsqueda</h1>
+            <hr/>
+            <div className="row">
+                <div className="col-5">
+                    <h4>Buscar</h4>
+                    <hr/>
+
+                    <form onSubmit={ handleSearch }>
+                        <input
+                            type="text"
+                            placeholder="Buscar un héroe"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                            value={ searchText }
+                            onChange={ handleInputChange }
+                        />
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary mt-1 btn-block"
+                        >
+                            Buscar
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
 
