3d59b41a10e5aeb94b07fc4b043cee73814aa7cc
LuisRcap <luisdany7773@gmail.com>
2022-07-31 21:05:07 -0500

    Pruebas de GifExpertApp :test_tube:

--- 04-gif-expert-app/src/tests/GifExpertApp.test.js
+++ 04-gif-expert-app/src/tests/GifExpertApp.test.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import GifExpertApp from '../GifExpertApp';
 
 describe('Pruebas en <GifExpertApp />', () => {
 
     test('Debe de mostrar el componente correctamente', () => {
-        const wrapper = shallow( <GifExpertApp /> );
-
-        expect( wrapper ).toMatchSnapshot();
+        const { container } = render( <GifExpertApp /> );
+        expect( container ).toMatchSnapshot();
     });
 
     test('Debe mostrar una lista de categorías', () => {
         
         const categories = ['One Punch', 'Dragon Ball'];
-        const wrapper = shallow(<GifExpertApp defaultCategories={ categories } />);
+        const { container } = render(<GifExpertApp defaultCategories={ categories } />);
 
-        expect( wrapper ).toMatchSnapshot();
-        expect( wrapper.find('GifGrid').length ).toBe( categories.length );
+        expect( container.innerHTML ).toContain('One Punch');
+        expect( container.innerHTML ).toContain('Dragon Ball');
+        
 
     });
     
