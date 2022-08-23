147d7bd876d84bb639b5ccfe1321419df140cc72
LuisRcap <luisdany7773@gmail.com>
2022-08-23 17:58:09 -0500

    Custom hook useForm

---
+++ 05-hook-app-vite/src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import { useForm } from "../hooks/useForm";
+
+const FormWithCustomHook = () => {
+
+    const { onInputChange, username, email, password } = useForm({
+        username: '',
+        email: '',
+        password: ''
+    });
+    
+
+    return (
+        <>
+            <h1>SimpleForm</h1>
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="user@example.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+
+            <input
+                type="password"
+                className="form-control mt-2"
+                placeholder="Contraseña"
+                name="password"
+                value={password}
+                onChange={onInputChange}
+            />
+
+        </>
+    );
+}
+
+export default FormWithCustomHook;
