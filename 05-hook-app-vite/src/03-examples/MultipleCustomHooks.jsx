7c1ea8b2d6ce0439415916ab36bb3aebbf4e0ca0
LuisRcap <luisdany7773@gmail.com>
2022-08-25 19:59:06 -0500

    MultipleHooks working :engine:

--- 05-hook-app-vite/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app-vite/src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,41 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch";
 
 const MultipleCustomHooks = () => {
 
-    const { data, isLoading, error } = useFetch( 'https://www.breakingbadapi.com/api/quotes/1' );
+    const { counter, increment } = useCounter(1);
+
+    const { data, isLoading, error } = useFetch( `https://www.breakingbadapi.com/api/quotes/${counter}` );
 
     console.log({ data, isLoading, error });
+
+    const { author, quote } = !!data && data[0];
 
     return (
         <>
             <h1>BreakingBad Quotes</h1>
             <hr />
+
+            {
+                isLoading ? (
+                    <div className="alert alert-info text-center" >
+                        Loading...
+                    </div>
+                ) : (
+                    <blockquote className="blockquote text-end" >
+                        <p className="mb-2" >{ quote }</p>
+                        <footer className="blockquote-footer" >{ author }</footer>
+                    </blockquote>
+                )
+            }
+
+            <button
+                className="btn btn-primary"
+                onClick={ () => increment() }
+                disabled={ isLoading }
+            >
+                Next quote
+            </button>
         </>
     )
 }
